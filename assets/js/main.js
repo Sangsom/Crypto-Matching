9bebe15dc0e67169c69f22f0754b29a38524ec3e
Rinalds Pudulis <rpudulis@gmail.com>
2018-03-06 16:48:22 +0200

    feat: Cryptocurrencies constant

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const cryptocurrencies = [
+  {
+    name: "Bitcoin",
+    id: "btc"
+  },
+  {
+    name: "Ethereum",
+    id: "eth"
+  },
+  {
+    name: "Ripple",
+    id: "xrp"
+  },
+  {
+    name: "Bitcoin Cash",
+    id: "bch"
+  },
+  {
+    name: "Litecoin",
+    id: "ltc"
+  },
+  {
+    name: "NEO",
+    id: "neo"
+  },
+  {
+    name: "Cardano",
+    id: "ada"
+  },
+  {
+    name: "Stellar",
+    id: "xlm"
+  }
+]
+
+console.log(cryptocurrencies);
