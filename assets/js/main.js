4eaf287dd20a78146f82350bb535b5d24a3ceb5f
Rinalds Pudulis <rpudulis@gmail.com>
2018-03-07 11:03:53 +0200

    feat: startGame function to generate new grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+
+const grid = document.getElementById('grid');
+let gameCards = [];
+
 const cryptocurrencies = [
   {
     name: "Bitcoin",
@@ -31,6 +35,42 @@ const cryptocurrencies = [
     name: "Stellar",
     id: "xlm"
   }
-]
+];
 
-console.log(cryptocurrencies);
+// Shuffle array
+const shuffleArray = arr =>
+  arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1]);
+
+
+// Reset the game grid and generate new cards
+const startGame = () => {
+  // Create grid array and shuffle it
+  gameCards = [];
+  gameCards.push(...cryptocurrencies, ...cryptocurrencies);
+  gameCards = shuffleArray(gameCards);
+
+  // Clear the grid
+  grid.innerHTML = "";
+
+  for (let i = 0; i < gameCards.length; i++) {
+    // Create variables
+    const gridBox = document.createElement('div');
+    const gridImg = document.createElement('img');
+
+    // Add class
+    gridBox.classList.add("grid-box");
+
+    // Set img attributes
+    gridImg.setAttribute('src', `/assets/svg/${gameCards[i].id}.svg`);
+    gridImg.setAttribute('alt', `${gameCards[i].name}`);
+
+    // Create html and append to #grid
+    gridBox.appendChild(gridImg);
+    grid.appendChild(gridBox);
+  }
+}
+
+
